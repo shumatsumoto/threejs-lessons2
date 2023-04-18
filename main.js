4f9ce81fd2360ff8198148e6380c410fe67d2554
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-04-18 18:04:16 +0900

    15. レンダラーを作成してみよう

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as THREE from "./build/three.module.js";
 
-let scene;
+let scene, camera, renderer;
 scene = new THREE.Scene();
 
-camara = new THREE.PerspectiveCamera(
+camera = new THREE.PerspectiveCamera(
   50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+
+renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
