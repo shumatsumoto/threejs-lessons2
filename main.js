cac43ba7e1bf17fb56872462876372025b1ac38e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-04-18 17:58:52 +0900

    13. カメラを作成してみよう

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ import * as THREE from "./build/three.module.js";
 
 let scene;
 scene = new THREE.Scene();
+
+camara = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
