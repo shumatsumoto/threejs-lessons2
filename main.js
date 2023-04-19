08feb17ff56a7e0c2afe3239d9d47da670554625
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-04-19 17:00:41 +0900

    22. ポイント光源を設置してみよう

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from "./build/three.module.js";
 
-let scene, camera, renderer;
+let scene, camera, renderer, pointLight;
 scene = new THREE.Scene();
 
 camera = new THREE.PerspectiveCamera(
@@ -33,6 +33,11 @@ scene.add(ballMesh);
 let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
+
+// ポイント光源を追加
+pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(-200, -200, -200);
+scene.add(pointLight);
 
 // レンダリング
 renderer.render(scene, camera);
